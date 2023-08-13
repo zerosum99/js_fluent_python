9d71c16580e60d06128684824cceb9f54614636f
Moon Yong Joon <myjlms99@gmail.com>
2023-08-13 15:44:12 +0900

    모듈 정의하기

---
+++ 파이썬_기초과정/main.py
@@ -0,0 +1,10 @@
+
+from math_utils import add, subtract
+
+result_add = add(5, 3)
+result_sub = subtract(10, 4)
+
+if __name__ == "__main__" : 
+
+    print("Addition:", result_add)
+    print("Subtraction:", result_sub)
