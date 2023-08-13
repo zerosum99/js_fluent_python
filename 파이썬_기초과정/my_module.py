607ac5389d38572b14d07c0900f84dc635425755
Moon Yong Joon <myjlms99@gmail.com>
2023-08-13 16:08:21 +0900

    파이썬 심화 구조 변경하기

---
+++ 파이썬_기초과정/my_module.py
@@ -0,0 +1,10 @@
+
+def my_function():
+    print("This is my function in my_module")
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+    def print_value(self):
+        print(f"My value is {self.value}")
